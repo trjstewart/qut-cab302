773e8eb362d69b85e7b770d0051cd45a1a232de8
Tylor Stewart <trjstewart@gmail.com>
2016-06-01 20:37:30 +1000

    Project Skeleton Source Code
    Initial source code provided by Jim Hogan for CAB302 Software Development - Assignment 2 (Version 8 as of 2016-05-25)

---
+++ Assignment 2 - Aircraft Simulator/AircraftSimulator/src/asgn2Simulators/GUISimulator.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ * This file is part of the AircraftSimulator Project, written as 
+ * part of the assessment for CAB302, semester 1, 2016. 
+ * 
+ */
+package asgn2Simulators;
+
+import java.awt.HeadlessException;
+
+import javax.swing.JFrame;
+
+/**
+ * @author hogan
+ *
+ */
+@SuppressWarnings("serial")
+public class GUISimulator extends JFrame implements Runnable {
+
+	/**
+	 * @param arg0
+	 * @throws HeadlessException
+	 */
+	public GUISimulator(String arg0) throws HeadlessException {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Runnable#run()
+	 */
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
